04fc0692766086dca284b0f8471a7bbf8fb96b98
x0myak <47006260+x0myak@users.noreply.github.com>
2019-06-10 17:55:43 +0300

    доработки

--- js/stat.js
+++ js/stat.js
@@ -1,31 +1,54 @@
+'use strict';
+
 var WINDOW_WIDTH = 420;
 var WINDOW_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var gap = 10;
+var GAP = 10;
+var COLUMN_GAP = 50;
+var COLUMN_WIDTH = 40;
+var TEXT_LEVEL = COLUMN_GAP + GAP * 4;
+var COLUMN_HEIGHT = 150;
+var COLUMN_X = CLOUD_X + GAP;
+var COLUMN_Y = COLUMN_GAP + GAP * 6;
+
 
 var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, WINDOW_WIDTH, WINDOW_HEIGHT);
 };
 
+var getMinElement = function(arr) {
+  var minElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] < minElement) {
+      minElement = arr[i];
+    }
+  }
+
+  return minElement;
+};
+
+var times = [1000, 2000, 4300, 2500];
+
 window.renderStatistics = function(ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + gap, CLOUD_Y + gap, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
 
   ctx.fillStyle = 'blue';
   ctx.font = "16px  PT Mono";
-  ctx.fillText('Ура вы победили!', 110, 75);
-  ctx.fillText('Список результатов:', 110, 75 + gap*2);
+  ctx.fillText('Ура вы победили!', COLUMN_X, COLUMN_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, COLUMN_GAP + GAP * 2)
 
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  var minTime = getMinElement(times);
 
-  // ctx.fillText('Вы', 110, 75);
-  // ctx.fillRect(160, 60, 430, 20);
+  var columnHeight = 150 * (minTime / times[i])
 
-  // ctx.fillText('Иван', 110, 105);
-  // ctx.fillRect(160, 90, 430, 20);
+  // var names = ['Вы', 'Иван', 'Юлия', 'bb'];
 
-  // ctx.fillText('Юлия', 110, 135);
-  // ctx.fillRect(160, 120, 430, 20);
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillText(names[i], COLUMN_X + (COLUMN_WIDTH + COLUMN_GAP) * i, TEXT_LEVEL);
+    ctx.fillRect(COLUMN_X + (COLUMN_WIDTH + COLUMN_GAP) * i, COLUMN_Y, COLUMN_WIDTH, columnHeight;
+  }
 };
