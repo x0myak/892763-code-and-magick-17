4f8bf69c17cd27616ae0ed9832db7fc00cef8135
x0myak <47006260+x0myak@users.noreply.github.com>
2019-06-14 16:18:52 +0300

    Поправила уровень колонок

--- js/stat.js
+++ js/stat.js
@@ -7,11 +7,11 @@ var CLOUD_Y = 10;
 var GAP = 10;
 var COLUMN_GAP = 50;
 var COLUMN_WIDTH = 40;
-var textLevel = COLUMN_GAP + GAP * 4;
+// var textLevel = COLUMN_GAP + GAP * 4;
 var MAX_COLUMN_HEIGHT = 150;
-var columnX = CLOUD_X + GAP;
-var columnY = COLUMN_GAP + GAP * 6;
-TEXT_Y = 50;
+var columnX = CLOUD_X +COLUMN_GAP;
+var columnY = CLOUD_Y + COLUMN_GAP * 5;
+var TEXT_Y = 50;
 
 
 var renderCloud = function(ctx, x, y, color) {
@@ -31,23 +31,31 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-var times = [1000, 2000, 4300, 2500];
+var getRandomColor = function () {
+  var randomColor = Math.random().toFixed(2);
+  return 'rgba(0, 0, 255, ' + randomColor + ')';
+};
+
+var drawRect = function (ctx, x, y, height, width, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, height, width);
+};
 
 window.renderStatistics = function(ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
 
-  ctx.fillStyle = 'blue';
+  ctx.fillStyle = 'gray';
   ctx.font = "16px  PT Mono";
   ctx.fillText('Ура вы победили!', columnX, TEXT_Y);
-  ctx.fillText('Список результатов:', columnX + GAP, COLUMN_GAP + GAP * 2)
+  ctx.fillText('Список результатов:', columnX, COLUMN_GAP + GAP * 2)
 
   var maxTime = getMaxElement(times);
 
-  // var names = ['Вы', 'Иван', 'Юлия', 'bb'];
-
   for (var i = 0; i < names.length; i++) {
-    ctx.fillText(names[i], columnX + (COLUMN_WIDTH + COLUMN_GAP) * i, textLevel);
-    ctx.fillRect(columnX + (COLUMN_WIDTH + COLUMN_GAP) * i, columnY, COLUMN_WIDTH, MAX_COLUMN_HEIGHT * (maxTime / times[i]));
+    var color = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : getRandomColor();
+    var x = columnX + (COLUMN_WIDTH + COLUMN_GAP) * i;
+    ctx.fillText(names[i], x, columnY + (MAX_COLUMN_HEIGHT - MAX_COLUMN_HEIGHT * times[i]) / maxTime - GAP - CLOUD_Y);
+    drawRect(ctx, x, columnY - GAP, COLUMN_WIDTH, (MAX_COLUMN_HEIGHT - MAX_COLUMN_HEIGHT * times[i]) / maxTime, color);
   };
 };
